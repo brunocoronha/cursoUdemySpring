7832996a5cd8896ef476776c716e207be7e4929f
brunocoronha <brunocoronha@gmail.com>
2024-03-12 13:36:55 -0300

    Product

--- curso/src/main/java/com/educandonaweb/curso/entities/Product.java
+++ curso/src/main/java/com/educandonaweb/curso/entities/Product.java
@@ -22,8 +22,8 @@ import lombok.ToString;
 @ToString
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -32,21 +32,27 @@ public class Category implements Serializable {
     private Long id;
 
     private String name;
-    
+    private String description;
+    private double price;
+    private String imgUrl;
+
     @Transient
     @Setter(AccessLevel.NONE)
-    private Set<Product> products = new HashSet<>();    
+    private Set<Category> categories = new HashSet<>();    
 
-    public Category(Long id, String name) {
+    public Product(Long id, String name, String description, double price, String imgUrl) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + (int) (id ^ (id >>> 32));
         return result;
     }
 
@@ -58,15 +64,13 @@ public class Category implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Category other = (Category) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
+        Product other = (Product) obj;
+        if (id != other.id)
             return false;
         return true;
     }
-    
+
+    //Licença office atribuida
     
 
 }
---
+++ curso/src/main/java/com/educandonaweb/curso/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.educandonaweb.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandonaweb.curso.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    
+} 
+ 
