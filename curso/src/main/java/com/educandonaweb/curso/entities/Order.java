7535072de50fd434b5c641c9a5b2b8d559863ee3
brunocoronha <brunocoronha@gmail.com>
2024-03-12 11:25:49 -0300

    OrderStatus enum

--- curso/src/main/java/com/educandonaweb/curso/entities/Order.java
+++ curso/src/main/java/com/educandonaweb/curso/entities/Order.java
@@ -3,9 +3,8 @@ package com.educandonaweb.curso.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.educandonaweb.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,36 +13,76 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 
 @Entity
 @Table(name = "td_order")
 public class Order implements Serializable {    
     
-    @EqualsAndHashCode.Include
-    @Id
+    private static final long serialVersionUID = 1L;
+
+    @Id    
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private static final long serialVersionUID = 1L;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
     
-    
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
     
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client){
+        super();
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
 
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus){
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }        
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
 }
